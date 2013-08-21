fe1b2b4798418693a8332ac006b0c12d09754bf2
vokaladmin <vokaladmin@vokaladmin-HP-Pavilion-Sleekbook-14-PC.(none)>
2013-08-21 12:44:13 +0300

    aasd

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,19 @@
-from fabric.api import local
+from __future__ import with_statement
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
 
 def pull():
-    local('git pull')
+    with settings(warn_only=True):
+        result = local('python login.py', capture=True)
+    if result.failed and not confirm("Tests failed. Continue anyway?"):
+        abort("Aborting at user request.")
+
+def test():
+    local('python login.py')
+
+def commit():
+    local("git add . && git commit -m 'aasd'")
+
+def deneme():
+    pull()
+    test()
